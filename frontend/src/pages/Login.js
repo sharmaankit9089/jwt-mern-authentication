45666c77e3c6246ee3ce9ad4dbb2931eb55f33fa
sharmaankit9089 <sharmaankit9089@gmail.com>
2025-11-18 23:25:13 +0530

    Update login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/login`;
+            const url = `https://jwt-mern-authentication-api.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
